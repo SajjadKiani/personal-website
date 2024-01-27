3738d37344f291cf6f4731b49b147cb744cc0662
saji <skm.kiani@gmail.com>
2024-01-27 14:45:56 +0330

    production url

--- src/service/instance.js
+++ src/service/instance.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: process.env.NODE_ENV === 'production'? 'https://sajadkiani.vercel.app/api' : 'http://localhost:3000/api'
 })
 
 export default instance;
