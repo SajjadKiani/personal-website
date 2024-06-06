7ffd6cdc716fb89c59843a0903d662e006a41b95
saji <skm.kiani@gmail.com>
2024-06-06 15:15:41 +0330

    mobile nav

--- src/components/layout/nav.jsx
+++ src/components/layout/nav.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Logo from "./logo";
+import { MenuIcon } from "lucide-react";
 
 
 export default function Nav () {
@@ -15,12 +16,16 @@ export default function Nav () {
         <nav className="flex container justify-between py-4 items-center">
             <Logo />
 
-            <div className="space-x-10">
+            <div className="md:block hidden space-x-10">
                 {links.map((item, index) => 
                     <Link className="text-[28px]" href={item.path} key={index}>
                         {item.title}
                     </Link>
                 )}
+            </div>
+
+            <div className="md:hidden block">
+                <MenuIcon />
             </div>
         </nav>
     )
