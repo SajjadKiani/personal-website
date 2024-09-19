3e6e4efeedd589ba762bd798a426bc8beac8d328
saji <skm.kiani@gmail.com>
2024-09-19 12:11:52 +0330

    update

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,22 +1,21 @@
 import Image from 'next/image'
-import authorImage from '@/public/images/authors/hamed.png'
+import authorImage from '@/public/images/authors/sajad.jpg'
 
 export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Hamed.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Saji.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Toronto, Canada. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+        and i&#39;m a Frontend Devloper
+        who loves blockchain technology and research. I enjoy building easy-to-use web3 solutions that combine advanced technology with good design.
         </p>
       </div>
       <div className='relative'>
         <Image
-          className='flex-1 rounded-lg grayscale'
+          className='flex-1 grayscale rounded-full shadow-lg'
           src={authorImage}
-          alt='Hamed Bahram'
+          alt='Sajad Kiani'
           width={175}
           height={175}
           priority
